ce272c4ddbd388a0da8745756a93b058ded134d3
matcool <26722564+matcool@users.noreply.github.com>
2024-12-26 00:19:22 -0300

    invalid

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+    std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found\n";
 }
