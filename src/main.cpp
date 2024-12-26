dfcda30beaae23247f20bd18a3d1e5e05c6072c4
matcool <26722564+matcool@users.noreply.github.com>
2024-12-26 00:35:31 -0300

    echo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+using namespace std::literals::string_view_literals;
+using namespace std::literals::string_view_literals;
+
 int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
@@ -12,8 +15,11 @@ int main() {
         std::string input;
         if (std::getline(std::cin, input).eof()) break;
 
-        if (input.starts_with("exit")) {
+        if (input.starts_with("exit"sv)) {
             return 0;
+        } else if (input.starts_with("echo "sv)) {
+            auto args = std::string_view(input).substr(5);
+            std::cout << args << "\n";
         } else {
             std::cout << input << ": command not found\n";
         }
