41471ffa4e2c53ce29b5d0d37b158578ed51f0e9
matcool <26722564+matcool@users.noreply.github.com>
2024-12-26 00:21:29 -0300

    repl

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@ int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    std::cout << "$ ";
 
-    std::string input;
-    std::getline(std::cin, input);
-    std::cout << input << ": command not found\n";
+    while (true) {
+        std::cout << "$ ";
+
+        std::string input;
+        if (std::getline(std::cin, input).eof()) break;
+        std::cout << input << ": command not found\n";
+    }
 }
